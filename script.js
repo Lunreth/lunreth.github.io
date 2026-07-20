88e7de8d6c776303ac04843e80f46ad7f9329c28
Lunreth <corsecondary@gmail.com>
2026-07-20 02:01:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const currentTheme = localStorage.getItem('theme');
 // Apply saved theme state on initialization
 if (currentTheme === 'dark') {
     document.documentElement.setAttribute('data-theme', 'dark');
-    themeToggle.textContent = '??';
+    themeToggle.textContent = '○ Tema Oscuro';
 } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    themeToggle.textContent = '??';
+    themeToggle.textContent = '☼ Tema Claro';
 }
 
 // Toggle logic execution
@@ -17,11 +17,11 @@ themeToggle.addEventListener('click', () => {
     
     if (theme === 'light') {
         document.documentElement.setAttribute('data-theme', 'dark');
-        themeToggle.textContent = '??';
+        themeToggle.textContent = '○ Tema Oscuro';
         localStorage.setItem('theme', 'dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        themeToggle.textContent = '??';
+        themeToggle.textContent = '☼ Tema Claro';
         localStorage.setItem('theme', 'light');
     }
 });
